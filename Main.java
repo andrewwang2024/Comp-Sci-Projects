5b60a07c86980d3ea81778afc4090c7254b7c7b7
andrewwang2024 <andrew.wang@sbs.org>
2021-10-17 00:35:51 +0000

    Added brainstorm for window rendering

--- Main.java
+++ Main.java
@@ -2,6 +2,17 @@ public class Main {
 
 	// TODO: Test method isShip (Will need to rename method)
 
+	/*
+	 *	Ideas for game board.
+	 *		- Make two boards on each side.
+	 *		- The first board (for the left side) is a never changing grid showing your ship
+	 * 		- The second board (for the left side) actually renders over the first board
+	 * 		- Rinse and repeat for the enemy board, where the static board is all white
+	 * 		- Alternatively, make the white board rendered over and slowly make the squares transparent,
+	 * 		  with the static board being the guess and ships.
+	 * 		- Side note, maybe make the ends of the ship more distinct with different colors?
+	 */
+
     public static void main(String[] args) {
 	    Player test = new Player(9, 100);
 	    for (int i = 0; i < test.getSize(); i++) {
